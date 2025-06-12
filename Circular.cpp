315253fb431bac4088ae8daac94e90462d624027
aryatama2004 <aryatama261@gmail.com>
2025-06-12 17:51:47 +0700

    mengiterasi dari front hingga akhir array

--- Circular.cpp
+++ Circular.cpp
@@ -95,7 +95,12 @@ public:
             }
             cout << endl;
         }
-   
+        else
+        {
+            // jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+            while (FRONT_position <= max - 1)
+    
+        }
   
     }
 };
