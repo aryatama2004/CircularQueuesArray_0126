0ead4bf78e6b0e4e8c5f20323aaeffe872c18f73
aryatama2004 <aryatama261@gmail.com>
2025-06-12 17:47:35 +0700

    mengecek antrian pada queues

--- Circular.cpp
+++ Circular.cpp
@@ -21,6 +21,20 @@ public:
         cout << "Enter a Number :";
         cin >> num;
         cout << endl;
+
+        // 1. cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n"; // 1.a
+            return;                       // 1.b
+        }
+
+
+        
     }
+
+ 
+
+
 };
 
