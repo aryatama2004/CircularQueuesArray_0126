ed80d302f27c678ece6302f26b6763be7d47e0f8
aryatama2004 <aryatama261@gmail.com>
2025-06-12 17:53:59 +0700

    memunculkan case

--- Circular.cpp
+++ Circular.cpp
@@ -145,7 +145,11 @@ int main()
                 q.remove();
                 break;
             }
- 
+            case '3':
+            {
+                q.display();
+                break;
+            }
 
             }
         }
