c5d4f160c4e309bdbe7d45546c30aa6dd308014b
aryatama2004 <aryatama261@gmail.com>
2025-06-12 17:49:11 +0700

    mengecek element jika memiliki 1

--- Circular.cpp
+++ Circular.cpp
@@ -56,7 +56,13 @@ public:
         }
         cout << "\nThe element deeted from the queue is : " << queue_array[FRONT] << "\n";
 
-
+        // cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+  
     }
 
 
