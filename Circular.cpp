23011028b01cad6228737615b694a242fc0ca141
aryatama2004 <aryatama261@gmail.com>
2025-06-12 17:52:17 +0700

    mengatur posisi dan menyimpan data

--- Circular.cpp
+++ Circular.cpp
@@ -104,7 +104,14 @@ public:
                 FRONT_position++;
             }
         }
-  
+        FRONT_position = 0;
+
+        while (FRONT_position <= REAR_position)
+        {
+            cout << queue_array[FRONT_position] << " ";
+            FRONT_position++;
+        }
+        cout << endl;
     }
 };
 
