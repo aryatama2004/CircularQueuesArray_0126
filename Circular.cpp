95a006d0ca65a4398eb9e1593d053abda8705594
aryatama2004 <aryatama261@gmail.com>
2025-06-12 17:48:52 +0700

    menghapus element jika antrian kosong

--- Circular.cpp
+++ Circular.cpp
@@ -46,7 +46,18 @@ public:
         queue_array[REAR] = num;
     }
 
- 
+    void remove()
+    {
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deeted from the queue is : " << queue_array[FRONT] << "\n";
+
+
+    }
 
 
 };
