3970ef73c7d5396c8d23e5eb016a2603a85bcad5
aryatama2004 <aryatama261@gmail.com>
2025-06-12 17:47:48 +0700

    mengecek antrian kosong

--- Circular.cpp
+++ Circular.cpp
@@ -29,7 +29,13 @@ public:
             return;                       // 1.b
         }
 
-
+        // 2. cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0; // 2.a
+            REAR = 0;  // 2.b
+        }
+     
         
     }
 
